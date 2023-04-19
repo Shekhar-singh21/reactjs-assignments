02f399f308ccdb4ac2805cd9b441ec92906404fa
shekhar <shekhar21k@gmail.com>
2023-04-19 22:24:35 +0530

    my assignment done

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const arr = ['Component based architecture','Virtual Dom for efficient update','Rich ecosystem and community ','Cross-platform development','strong community support']
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+        <h1>Benifits of using react...</h1>
+        <ol>
+
+        {arr.map(item=>
+        <li key={item}>{item}</li>
+        
+        )}         
+        </ol>
+        <button>Get Started...</button>
       </header>
     </div>
   );
