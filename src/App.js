fbdfe1bf8e023bb8be6bc886050d13358a70fe89
shekhar <shekhar21k@gmail.com>
2023-04-25 00:53:07 +0530

    created a component also pass data to child component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ProfileCard from './profileCard';
+
+function App() {
+  return (
+    <div className="App">
+    <ProfileCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/profileCard.js
@@ -0,0 +1,41 @@
+import { Fragment } from "react";
+import './profileCard.css';
+
+
+export default function ProfileCard(){
+
+
+
+    return(
+
+        <Fragment>
+
+        <section className="card">
+
+        <div>
+
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkAgOzvDKZ0aPqRUW7erk9hrNrl57zKwEZiw&usqp=CAU" alt="not available"  className="img1"/>
+
+
+
+
+        </div>
+
+
+
+            
+
+
+        </section>
+
+
+
+
+        </Fragment>
+
+
+
+    )
+
+
+}
