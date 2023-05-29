91c358c760b877e9409984bd535bf9f8dd578d50
shekhar <shekhar21k@gmail.com>
2023-05-29 22:30:11 +0530

    boilerPlate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Slider } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
+    <Slider
+  size="small"
+  defaultValue={70}
+  aria-label="Small"
+  valueLabelDisplay="auto"
+/>
+<Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
     
     </div>
   );
